1345d76976883eb36439eb216e227a8f5476ffd2
Arvid Malm <arvidmalm@Arvids-MacBook-Air.local>
2024-09-25 23:06:34 +0200

    logo zoom

--- project/src/Navbar.jsx
+++ project/src/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container">
         <Link className="navbar-brand" to="/">
-          <img src={logo} alt="Arvid & Samuel" />
+          <img src={logo} alt="Arvid & Samuel"/>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
